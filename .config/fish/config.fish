38952e3437afa710f7d4a23e250542ff1bf90bb9
Connor Azzarello <connor.azzarello@gmail.com>
2021-05-26 04:09:32 -0700

    I guess I am pushing fish_vars now...will investigate

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ fish_vi_key_bindings
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/corndog/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+eval ~/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 set -x BAT_THEME Nord
 #set -x PAGER "/bin/sh -c \"unset PAGER;col -b -x | \
